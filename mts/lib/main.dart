71e4d57b2181a0c326dd2d9900aabe7a34ada2e5
kajigin <222111089gk@tama.ac.jp>
2022-10-10 14:30:02 +0900

    todoapp

--- mts/lib/main.dart
+++ mts/lib/main.dart
@@ -1,83 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  // 最初に表示するWidget
+  runApp(MyTodoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class MyTodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      // アプリ名
+      title: 'My Todo App',
       theme: ThemeData(
+        // テーマカラー
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // リスト一覧画面を表示
+      home: TodoListPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0; //変数定義１
-  String _type = "偶数";
-
-  void _incrementCounter() {
-    setState(() {
-      //こいつで画面を更新
-      _counter++;
-      if (_counter % 2 == 0) {
-        _type = "偶数";
-      } else {
-        _type = "奇数";
-      } //setStateの中で変数の更新４
-    });
-  }
-
+// リスト一覧画面用Widget
+class TodoListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter', //変数の表示２
-              style: Theme.of(context).textTheme.headline4, //変数の更新５
-            ),
-            Text('$_type', style: TextStyle(fontSize: 20, color: Colors.blue))
-          ],
+        child: Text('リスト一覧画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            return TodoAddPage();
+          }));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TodoAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('リスト追加画面(クリックで戻る)'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter, //ボタンを押すと関数が呼ばれる３
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter, //ボタンを押すと関数が呼ばれる３
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
