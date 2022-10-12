ae5b5c908f76908b94b0c66f796a7fe6cc4c09f1
kajigin <95005425+kajigin@users.noreply.github.com>
2022-10-12 11:30:50 +0900

    tuduki

--- mts/lib/main.dart
+++ mts/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   // 最初に表示するWidget
@@ -9,6 +12,7 @@ class MyTodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // アプリ名
       title: 'My Todo App',
       theme: ThemeData(
@@ -21,19 +25,43 @@ class MyTodoApp extends StatelessWidget {
   }
 }
 
-// リスト一覧画面用Widget
-class TodoListPage extends StatelessWidget {
+// リスト一覧画面用Widgetclass TodoListPage extends StatefulWidget {
+class TodoListPage extends StatefulWidget {
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  List<String> todoList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('リスト一覧画面'),
+      appBar: AppBar(
+        title: Text('リスト一覧'),
+      ),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(todoList[index]),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-            return TodoAddPage();
-          }));
+        onPressed: () async {
+          final newListText = await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return TodoListPage();
+            }),
+          );
+          if (newListText != null) {
+            //キャンセルするとnewListTextがnullになる
+            setState(() {
+              todoList.add(newListText);
+            });
+          }
         },
         child: Icon(Icons.add),
       ),
@@ -41,18 +69,60 @@ class TodoListPage extends StatelessWidget {
   }
 }
 
-class TodoAddPage extends StatelessWidget {
+//リスト追加画面用widget
+class TodoAddPage extends StatefulWidget {
+  @override
+  _TodoAddPageState creatState() => _TodoAddPageState();
+}
+
+class _TodoAddPageState extends State<TodoAddPage> {
+  String _text = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text('リスト追加画面(クリックで戻る)'),
+        appBar: AppBar(
+          title: Text('リスト追加'),
         ),
-      ),
-    );
+        body: Container(
+          padding: EdgeInsets.all(64), //余白調整
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(_text, style: TextStyle(color: Colors.blue)),
+              const SizedBox(height: 8),
+              TextField(
+                onChanged: (String value) {
+                  setState(() {
+                    _text = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 8),
+              Container(
+                width: double.infinity, //横幅いっぱい
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue, //
+                    onPrimary: Colors.white,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop(_text);
+                  },
+                  child: Text('リスト追加'),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Container(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('キャンセル'),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
